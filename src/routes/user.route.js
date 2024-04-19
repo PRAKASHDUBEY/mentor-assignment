86ef127f62f6ee3bca70790ae33218a03516bb86
Prakash Dubey <prakashdubey@Prakashs-MacBook-Pro.local>
2024-04-19 14:35:37 +0530

    Add auth middleware and routes

---
+++ src/routes/user.route.js
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import { 
+    registerUser, 
+    verifyOTP,
+    loginUser, 
+    logoutUser, 
+    getCurrentUser, 
+    changeCurrentPassword, 
+    updateAccountDetails
+} from "../controllers/user.controller.js";
+
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router = Router()
+
+
+router.route("/register").post(registerUser)
+router.route("/verify-otp").post(verifyOTP)
+router.route("/login").post(loginUser)
+
+// //secured routes
+router.route("/logout").post(verifyJWT,  logoutUser)
+router.route("/profile").get(verifyJWT, getCurrentUser)
+router.route("/change-password").patch(verifyJWT, changeCurrentPassword)
+router.route("/update").patch(verifyJWT, updateAccountDetails)
+
+export default router
